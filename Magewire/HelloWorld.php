946917e9c13b02cf6445cdeda5092cd2ba0ae021
jisse Reitsma <jisse@yireo.com>
2023-12-12 09:26:57 +0100

    Adding all files to git via Yireo Command

---
+++ Magewire/HelloWorld.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace YireoTraining\MageWireBackendHelloWorld\Magewire;
+
+use Magewirephp\Magewire\Component;
+
+class HelloWorld extends Component
+{
+    public string $helloWorld = 'Hello World';
+}
